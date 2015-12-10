94ac5cfbd1715e7465d73dd5d14961b5994446dd
yubousa10 <yubousa10@163.com>
2015-12-10 15:05:12 -0500

    add checkout ticket info

--- postManager/views.py
+++ postManager/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from checkout.models import Ticket
+from postManager.models import PostBase
 
 
 # Create your views here.
 @login_required(login_url='/')
 def activity_detail(request,activity_id):
 	template_name = 'activity_detail.html'
-	context = {}
+	postObject = PostBase.objects.get(pk=activity_id)
+	
+	tickets = Ticket.objects.filter(post_id = activity_id)
+	context = {
+	'tickets': tickets,
+	'postObject' : postObject,
+	}
 	return render(request,template_name,context)
